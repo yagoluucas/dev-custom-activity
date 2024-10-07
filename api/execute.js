4cae931a312f280a6b3e68e1ea05e4ab164f1a6d
Yago Lucas <-v>
2024-10-07 16:15:55 -0300

    feito primeira rota usando o verbo http get

---
+++ api/execute.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const rotas = require('../routes/routes.js');
+
+const app = express();
+
+const port = 8000
+
+app.use('/api', rotas);
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
